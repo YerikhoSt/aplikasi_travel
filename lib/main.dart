0d833e1eb5810d4fa9893ddfc62b60bec457d6a5
YerikhoS <yerikhosutandi0@gmail.com>
2022-04-07 13:05:54 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:aplikasi_travel/ui/pages/bonus_page.dart';
+import 'package:aplikasi_travel/ui/pages/get_started_page.dart';
+import 'package:aplikasi_travel/ui/pages/sign_up_page.dart';
+import 'package:aplikasi_travel/ui/pages/splash.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      routes: {
+        '/':(context) => const SplashPage(),
+        '/get-started':(context) => const GetStartedPage(),
+        '/sign-up':(context) => const SignUpPage(),
+        '/bonus-page':(context) => const BonusPage(),
+      },
+    );
+  }
+}
---
+++ lib/ui/pages/sign_up_page.dart
@@ -0,0 +1,244 @@
+import 'package:flutter/material.dart';
+import '../../shared/theme.dart';
+
+class SignUpPage extends StatelessWidget {
+  const SignUpPage({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Widget title() {
+      return Container(
+        margin: const EdgeInsets.only(top: 30,),
+        child: Text(
+          'Join us and get\nyour next journey',
+          style: blackTextStyle.copyWith(
+            fontSize: 24,
+            fontWeight: semiBold,
+          ),
+        ),
+      );
+    }
+
+    Widget inputSection() {
+
+      Widget nameInput() {
+        return Container(
+          margin: const EdgeInsets.only(bottom: 20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Full Name',
+                style: blackTextStyle,
+              ),
+              const SizedBox(height: 6,),
+              TextFormField(
+                cursorColor: kBlackColor,
+                decoration: InputDecoration(
+                  hintText: 'Your Full Name',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(
+                      defaultRadius,
+                    ),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(
+                      defaultRadius,
+                    ),
+                    borderSide: BorderSide(
+                      color: kPrimaryColor,
+                    )
+                  ),
+                ),
+              ),
+            ],
+          ),
+        );
+      }
+
+      Widget emailInput() {
+        return Container(
+          margin: const EdgeInsets.only(bottom: 20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Email Address',
+                style: blackTextStyle,
+              ),
+              const SizedBox(height: 6,),
+              TextFormField(
+                cursorColor: kBlackColor,
+                decoration: InputDecoration(
+                  hintText: 'Your Email',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(
+                      defaultRadius,
+                    ),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(
+                      defaultRadius,
+                    ),
+                    borderSide: BorderSide(
+                      color: kPrimaryColor,
+                    )
+                  ),
+                ),
+              ),
+            ],
+          ),
+        );
+      }
+
+        Widget passwordInput() {
+        return Container(
+          margin: const EdgeInsets.only(bottom: 20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Password',
+                style: blackTextStyle,
+              ),
+              const SizedBox(height: 6,),
+              TextFormField(
+                obscureText: true,
+                cursorColor: kBlackColor,
+                decoration: InputDecoration(
+                  hintText: 'Your Password',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(
+                      defaultRadius,
+                    ),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(
+                      defaultRadius,
+                    ),
+                    borderSide: BorderSide(
+                      color: kPrimaryColor,
+                    )
+                  ),
+                ),
+              ),
+            ],
+          ),
+        );
+      }
+
+        Widget hobbyInput() {
+        return Container(
+          margin: const EdgeInsets.only(bottom: 30),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Hobby',
+                style: blackTextStyle,
+              ),
+              const SizedBox(height: 6,),
+              TextFormField(
+                cursorColor: kBlackColor,
+                decoration: InputDecoration(
+                  hintText: 'Your Hobby',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(
+                      defaultRadius,
+                    ),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(
+                      defaultRadius,
+                    ),
+                    borderSide: BorderSide(
+                      color: kPrimaryColor,
+                    )
+                  ),
+                ),
+              ),
+            ],
+          ),
+        );
+      }
+
+      Widget submitButton() {
+        return Container(
+          width: double.infinity,
+          height: 55,
+          child: TextButton(
+            style: TextButton.styleFrom(
+              backgroundColor: kPrimaryColor,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(defaultRadius)
+              ),
+            ),
+            onPressed: (){
+              Navigator.pushNamed(context, '/bonus-page');
+            }, 
+            child: Text(
+              'Sign Up',
+              style: whiteTextStyle.copyWith(
+                fontSize: 18,
+                fontWeight: medium,
+              ),    
+            ),
+          ),
+        );
+      }
+
+      return Container(
+        margin: const EdgeInsets.only(top: 30),
+        padding: const EdgeInsets.symmetric(
+          horizontal: 20,
+          vertical: 0,
+        ),
+        decoration: BoxDecoration(
+          color: kWhiteColor,
+          borderRadius: BorderRadius.circular(
+            defaultRadius,
+          ),
+        ),
+        child: Column(
+          children: [
+            nameInput(),
+            emailInput(),
+            passwordInput(),
+            hobbyInput(),
+            submitButton()
+          ],
+        ),
+      );
+    }
+
+    Widget tacButton() {
+      return Container(
+        alignment: Alignment.center,
+        margin: const EdgeInsets.only(top: 50, bottom: 73,),
+        child: Text(
+          'Terms and Conditions',
+          style: greyTextStyle.copyWith(
+            fontSize: 16,
+            fontWeight: light,
+            decoration: TextDecoration.underline,
+          ),
+        ),
+      );
+    }
+    return Scaffold(
+      backgroundColor: kBackgroundColor,
+      body: SafeArea(
+        child: ListView(
+          padding: EdgeInsets.symmetric(
+            horizontal: defaultMargin
+          ),
+          children: [
+            title(),
+            inputSection(),
+            tacButton()
+            ],
+        ),
+      ),
+    );
+  }
+}
